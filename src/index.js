8283848d17e7ce905c6d0bd23e55cc08518a6ba8
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-10-16 14:08:30 +0100

    Starting to work on index styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('testing, testing');
+import './style.css';
+import NeelixHeaderImg from './neelix-header.jpeg';
+
+
+
+function component() {
+    const element = document.createElement('div');
+
+    const NeelixHeader = new Image();
+    NeelixHeader.src = NeelixHeaderImg;
+    NeelixHeader.className = "header-image";
+
+    element.appendChild(NeelixHeader);
+
+    return element;
+}
+
+document.body.appendChild(component());
+
+
