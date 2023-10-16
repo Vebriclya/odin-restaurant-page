5f264f95c50005d0bdb5439f64f36aaa5577a2a7
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-10-16 18:14:45 +0100

    Added some styling and divs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import './style.css';
-import NeelixHeaderImg from './neelix-header.jpeg';
+import WelcomeLeft from './neeliximg.png';
+import ScanLines from './scan-7133901_1920.png';
 
 
 
 function component() {
     const element = document.createElement('div');
+    element.className = "window-div";
 
-    const NeelixHeader = new Image();
-    NeelixHeader.src = NeelixHeaderImg;
-    NeelixHeader.className = "header-image";
+    const borderControl = document.createElement('div');
+    borderControl.className = 'border-control';
 
-    element.appendChild(NeelixHeader);
+    const pageContainer = document.createElement('div');
+    pageContainer.className = 'page-container';
+
+    element.appendChild(borderControl)
+    borderControl.appendChild(pageContainer);
+
+    const WelcomeImage = new Image();
+    WelcomeImage.src = WelcomeLeft;
+    WelcomeImage.className = "welcome-image";
+
+    pageContainer.appendChild(WelcomeImage);
+
+    
+    const ScanLineOverlay = new Image();
+    ScanLineOverlay.src = ScanLines;
+    ScanLineOverlay.className = 'scan-lines';
+
+    pageContainer.appendChild(ScanLineOverlay);
 
     return element;
 }
