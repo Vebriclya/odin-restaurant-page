8a650dcb52c5a4584f1b76d5efc52490acd6cea0
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-10-17 18:58:25 +0100

    slight changes to css

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
 export function homePage() {
     const textArea = document.querySelector('.text-area');
-    textArea.innerHTML = '<br><b>Hello, my friends from across the stars!</b> <br><br> Neelix here, your one and only Talaxian culinary genius and host extraordinaire. <br><br> If there\'s one thing I\'ve learned from my adventures aboard the U.S.S. Voyager, it\'s that food has the power to bring people together, no matter where they come from. <br><br> That\'s why I\'m delighted to welcome you to <b>\"Neelix\'s Nebula Bistro,\"</b> the galaxy\'s most exciting and diverse culinary destination.';
+    textArea.innerHTML = '<br><b class="menu-headers">Hello, my friends from across the stars!</b> <br><br> Neelix here, your one and only Talaxian culinary genius and host extraordinaire. <br><br> If there\'s one thing I\'ve learned from my adventures aboard the U.S.S. Voyager, it\'s that food has the power to bring people together, no matter where they come from. <br><br> That\'s why I\'m delighted to welcome you to <b>\"Neelix\'s Nebula Bistro,\"</b> the galaxy\'s most exciting and diverse culinary destination.';
 }
